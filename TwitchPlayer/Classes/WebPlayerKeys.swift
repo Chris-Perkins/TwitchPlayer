82f596a301ba0ebe57136fb2a67e60d132e44798
chrisfromtemporaryid@gmail.com <chrisfromtemporaryid@gmail.com>
2019-01-14 10:30:05 -0500

    Add missing Web Player Keys file

---
+++ TwitchPlayer/Classes/WebPlayerKeys.swift
@@ -0,0 +1,28 @@
+//
+//  WebPlayerKeys.swift
+//  TwitchPlayer
+//
+//  Created by Christopher Perkins on 1/14/19.
+//
+
+/// `TwitchWebPlayerKeys` specifies the different keys that are a part of initialization for a Twitch Player.
+internal struct TwitchWebPlayerKeys {
+    internal static let allowsFullScreen = "allowfullscreen"
+    internal static let autoplay = "autoplay"
+    internal static let chatMode = "chat"
+    internal static let channel = "channel"
+    internal static let clip = "clip"
+    internal static let collection = "collection"
+    internal static let fontSize = "font-size"
+    internal static let height = "height"
+    internal static let layout = "layout"
+    internal static let muted = "muted"
+    internal static let preload = "preload"
+    internal static let scrolling = "scrolling"
+    internal static let theme = "theme"
+    internal static let video = "video"
+    internal static let width = "width"
+    
+    /// Uninitializable
+    private init() { }
+}
